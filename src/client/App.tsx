c37a2638832bf3cd064327796f286d700ffa8e3f
chelinaobiang <166460631+chelinaobiang@users.noreply.github.com>
2025-10-16 19:57:05 -0700

    Update App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -5,7 +5,7 @@ export const App = () => {
   const { count, username, loading, increment, decrement } = useCounter();
   return (
     <div className="flex relative flex-col justify-center items-center min-h-screen gap-4">
-      <img className="object-contain w-1/2 max-w-[250px] mx-auto" src="/snoo.png" alt="Snoo" />
+      <img className="object-contain w-1/2 max-w-[250px] mx-auto" src="/SimulifeLogo.png" alt="Snoo" />
       <div className="flex flex-col items-center gap-2">
         <h1 className="text-2xl font-bold text-center text-gray-900 ">
           Welcome to simuliv3
